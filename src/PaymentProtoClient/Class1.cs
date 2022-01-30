b2fc44ba301539e72c43b929610542d974840e03
Marius <marius_taboo@yahoo.com>
2022-01-30 19:24:03 +0000

    add client package

---
+++ src/PaymentProtoClient/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Payment;
+
+namespace PaymentProtoClient
+{
+    internal class Consumer
+    {
+        public void CLientApplication()
+        {
+            /// 
+            // Channel and client need to be implemented in the client app
+            // Grpc.AspNetCore package is not compatible with netstandard2.1
+            ///
+
+            // using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            // var client =  new InitiateTransaction.InitiateTransactionClient(channel);
+
+            // var resonse = client.Create(new TransactionMessage
+            // {
+            //     TransactionId = Guid.NewGuid().ToString(),
+            //     Description = "test description"
+            // });
+        }
+    }
+}
